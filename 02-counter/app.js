a08d74bd3ac0d91163a6c627274a190a0aa3aa49
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-12-16 20:10:14 -0300

    Finalizar listeners com mudanca do contador e do valor html do elemento

--- 02-counter/app.js
+++ 02-counter/app.js
@@ -1,20 +1,32 @@
-let value = document.getElementById('value');
+// set initial count
+let count = 0;
+
+const htmlValue = document.getElementById('value');
 
 const decreaseBtn = document.querySelector('.decrease');
 const resetBtn = document.querySelector('.reset');
-const increaseBtn = document.querySelector('increase');
+const increaseBtn = document.querySelector('.increase');
 
 
 decreaseBtn.addEventListener('click', function() {
+	count -= 1;
+	console.log(count);
 	
+	htmlValue.textContent = count;
 });
 
 resetBtn.addEventListener('click', function() {
+	count = 0;
+	console.log(count);
 	
+	htmlValue.textContent = count;
 });
 
 increaseBtn.addEventListener('click', function() {
+	count += 1;
+	console.log(count);
 	
+	htmlValue.textContent = count;
 });
 
 
