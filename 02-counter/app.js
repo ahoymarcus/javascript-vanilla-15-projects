7dc498acc1a1252fd96afa18aa556b1254725b63
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-12-16 20:04:42 -0300

    Criar variaveis para os elementos html e criar listeners para botoes

---
+++ 02-counter/app.js
@@ -0,0 +1,24 @@
+let value = document.getElementById('value');
+
+const decreaseBtn = document.querySelector('.decrease');
+const resetBtn = document.querySelector('.reset');
+const increaseBtn = document.querySelector('increase');
+
+
+decreaseBtn.addEventListener('click', function() {
+	
+});
+
+resetBtn.addEventListener('click', function() {
+	
+});
+
+increaseBtn.addEventListener('click', function() {
+	
+});
+
+
+
+
+
+
