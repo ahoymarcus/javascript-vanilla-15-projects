590f4ed1019618e5a05d085ed4a73c8c3734ef61
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-12-17 19:33:55 -0300

    Criar funcao de listener para fazer o toggle do sidebar e comentar o script styles.css

--- 05-sidebar/app.js
+++ 05-sidebar/app.js
@@ -1,10 +1,20 @@
 // https://www.youtube.com/watch?v=c5SIG7Ie0dM
 // 1 hs  25'  34''
 
+const sidebar = document.querySelector('.sidebar');
+const sideBarToggle = document.querySelector('.sidebar-toggle');
+const closeBtn = document.querySelector('.close-btn');
 
 
 
-
+//show-sidebar
+sideBarToggle.addEventListener('click', function() {
+	//console.log(sidebar.classList);
+	const cssClassTest = sidebar.classList.contains('show-sidebar');
+	console.log(cssClassTest);
+	
+	cssClassTest ? sidebar.classList.remove('show-sidebar') : sidebar.classList.add('show-sidebar');
+});
 
 
 
