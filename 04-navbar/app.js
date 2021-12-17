471db34861454e516cab0646686cd36794f5c474
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-12-17 18:26:10 -0300

    Descomentar classe CSS em styles.css e criar variaveis para elementos html e criar tambem listener para tirar o colocar classe css em elemento de links.

--- 04-navbar/app.js
+++ 04-navbar/app.js
@@ -6,6 +6,34 @@
 // remove - remove class
 // toggle - toggles class
 
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+
+
+navToggle.addEventListener('click', function() {
+	console.log(links.classList);
+	
+	const cssClassTest = links.classList.contains('show-links');
+	console.log(cssClassTest);
+	
+	if (!cssClassTest) {
+		links.classList.add('show-links');
+	} else {
+		links.classList.remove('show-links');
+	}
+});
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
