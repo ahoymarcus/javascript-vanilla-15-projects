ddcacf2b2253ebfd82d4aef92510b50efab6511a
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-12-17 22:04:36 -0300

    Criar funcionalidade para fechamento de todos os cartoes de perguntas, exceto aquele clicado pelo usuario.

--- 07-questions/app (version-with-inside-selectors).js
+++ 07-questions/app (version-with-inside-selectors).js
@@ -33,23 +33,4 @@ questions.forEach(function(question) {
 
 
 
-// Resolução 1
-// traversing the dom
-// const questionBtns = document.querySelectorAll('.question-btn');
-
-// questionBtns.forEach(function(btn) {
-	// btn.addEventListener('click', function(e) {
-		////console.log(e.currentTarget);
-		
-		// const specificQuestion = e.currentTarget.parentElement.parentElement;
-		// console.log('Traversing the DOM... ', specificQuestion);
-		
-		
-		// specificQuestion.classList.toggle('show-text');
-	// });
-// });
-
-
-
-
 
