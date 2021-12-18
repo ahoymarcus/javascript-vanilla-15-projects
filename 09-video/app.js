8f7c66b32918e7e6b7d5c948b6d7723ea8fdec50
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-12-18 19:56:25 -0300

    Criar as variaveis para os elemetnos html. Adicionar listener ao botao de controle do video e mudar a cor CSS e setar play ou pause ao video.

--- 09-video/app.js
+++ 09-video/app.js
@@ -4,12 +4,27 @@
 // The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images, and subframes to finish loading.
 // The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images.
 
+const video = document.querySelector('.video-container');
+const switchBtn = document.querySelector('.switch-btn');
 
 
 
-
-
-
+switchBtn.addEventListener('click', function() {
+	console.log(switchBtn);
+	console.log(switchBtn.classList);
+	
+	// test the CSS control over the button position
+	// CSS class: .slide .switch
+	if (!switchBtn.classList.contains('slide')) {
+		switchBtn.classList.add('slide');
+		
+		video.pause();
+	} else {
+		switchBtn.classList.remove('slide');
+		
+		video.play();
+	}
+});
 
 
 
