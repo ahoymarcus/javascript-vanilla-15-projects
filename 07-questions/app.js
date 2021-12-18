6c3e798e7fbf7975b8167e4e74dd94813ff432b2
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-12-17 21:25:38 -0300

    Selecionar Nodelist de botoes. Iterar com forEach criando listener para cada btn na lista

--- 07-questions/app.js
+++ 07-questions/app.js
@@ -3,13 +3,33 @@
 //using selectors inside the element
 // traversing the dom
 
+const plusBtn = document.querySelector('.plus-icon');
+const minusBtn = document.querySelector('.minus-icon');
+
+const questionBtns = document.querySelectorAll('.question-btn');
+const singleQuestion = document.querySelector('.question');
+const questionText = document.querySelector('.question-text');
+
+
+
+questionBtns.forEach(function(btn) {
+	btn.addEventListener('click', function(e) {
+		console.log(e.currentTarget);
+	});
+});
 
 
 
 
+// CSS class: show-text
+// CSS class: question-text
+// plusBtn.addEventListener('click', function(e) {
+	// singleQuestion.classList.add('show-text');
+// });
 
-
-
+// minusBtn.addEventListener('click', function(e) {
+	// singleQuestion.classList.remove('show-text');
+// });
 
 
 
