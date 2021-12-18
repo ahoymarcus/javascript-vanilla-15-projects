98f9dc342cd187e8f19a1dc26311832d8454d219
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-12-17 21:39:00 -0300

    A partir do botao especifico selecionado, Atravessar o DOM ate o elemento avo e toggar a classe css correta

--- 07-questions/app.js
+++ 07-questions/app.js
@@ -1,35 +1,28 @@
 // https://www.youtube.com/watch?v=c5SIG7Ie0dM
-// 1 hs  56'  06''
+// 2 hs  06'  06''
 //using selectors inside the element
 // traversing the dom
 
-const plusBtn = document.querySelector('.plus-icon');
-const minusBtn = document.querySelector('.minus-icon');
+
 
 const questionBtns = document.querySelectorAll('.question-btn');
-const singleQuestion = document.querySelector('.question');
-const questionText = document.querySelector('.question-text');
+
+
 
 
 
 questionBtns.forEach(function(btn) {
 	btn.addEventListener('click', function(e) {
-		console.log(e.currentTarget);
+		//console.log(e.currentTarget);
+		
+		const specificQuestion = e.currentTarget.parentElement.parentElement;
+		console.log('Traversing the DOM... ', specificQuestion);
+		
+		
+		specificQuestion.classList.toggle('show-text');
 	});
 });
 
-
-
-
-// CSS class: show-text
-// CSS class: question-text
-// plusBtn.addEventListener('click', function(e) {
-	// singleQuestion.classList.add('show-text');
-// });
-
-// minusBtn.addEventListener('click', function(e) {
-	// singleQuestion.classList.remove('show-text');
-// });
 
 
 
