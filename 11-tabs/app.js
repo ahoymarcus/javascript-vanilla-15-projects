a49e154dd473bc5833750cb4c5d68c09ffe5501f
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-12-20 19:33:22 -0300

    Criar variaveis para referencia dos elementos HTML. Criar listerner nos botoes para mudar classe CSS  de controle.

--- 11-tabs/app.js
+++ 11-tabs/app.js
@@ -1,7 +1,32 @@
 // https://www.youtube.com/watch?v=c5SIG7Ie0dM
-// 3 hs  36'  10''
+// 4 hs  48'  10''
+
+const aboutContainer = document.querySelector('.about');
+const tabBtns = document.querySelectorAll('.tab-btn');
+const articles = document.querySelectorAll('.content');
 
 
+aboutContainer.addEventListener('click', function(e) {
+	console.log(e.target);
+	console.log('dataset.id = ', e.target.dataset.id);
+	
+	const id = e.target.dataset.id;
+	
+	if (id) {
+		// remove active from all tabBtns
+		tabBtns.forEach(function(btn) {
+			// remove classe Active from all
+			btn.classList.remove('active');
+			// give class Active to clicked btn
+			e.target.classList.add('active');
+		});
+		
+		
+		
+		
+	}
+	
+});
 
 
 
