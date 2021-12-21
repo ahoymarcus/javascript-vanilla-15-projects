c5e464f97308deda319e0764a82886a206fb3aa3
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-12-21 20:49:07 -0300

    Criar 07 variaveis para elementos html e outras 03 para edicao. Criar listener em formulario e mocar um ID para os itens de lista.

--- 14-grocery/app.js
+++ 14-grocery/app.js
@@ -2,12 +2,39 @@
 // 6 hs  26'  30''
 
 // ****** SELECT ITEMS **********
+const alert = document.querySelector('.alert');
+const form = document.querySelector('.grocery-form');
+const groceryInput = document.getElementById('grocery');
+const submitBtn = document.querySelector('.submit-btn');
+const container = document.querySelector('.grocery-container');
+const list = document.querySelector('.grocery-list');
+const clearBtn = document.querySelector('.clear-btn');
 
 // edit option
+let editElement;
+let editFlag = false;
+let editID = "";
 
 // ****** EVENT LISTENERS **********
+// submit form
+form.addEventListener('submit', addItem);
+
+
 
 // ****** FUNCTIONS **********
+function addItem(e) {
+	e.preventDefault();
+	
+	const value = groceryInput.value;
+	console.log(value);
+	
+	// mock a ID with milliseconds
+	const id = new Date().getTime().toString();
+	console.log('mock_ID = ', id);
+	
+};
+
+
 
 // ****** LOCAL STORAGE **********
 
