7ae5279774e35ce0e789ca0e734def8e4712028a
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-12-20 21:10:16 -0300

    Terminar extracao dos valores individuais do objeto de data

--- 12-countdown/app.js
+++ 12-countdown/app.js
@@ -1,5 +1,5 @@
 // https://www.youtube.com/watch?v=c5SIG7Ie0dM
-// 5 hs  18'  50''
+// 5 hs  24'  30''
 const months = [
   "January",
   "February",
@@ -32,20 +32,30 @@ const timeBits = document.querySelectorAll('.deadline-format h4');
 
 /*
 	Careful, because month param is 0 indexed...
+	-> Params
+	year, month, day, minutes, seconds, milliseconds
 */
-let futureDate = new Date(2020, 4, 24, 15, 30, 0);
+let futureDate = new Date(2021, 12, 25, 0, 0, 0);
 console.log(futureDate);
 
 // the easy picks
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
-const minutes = futureDate.getMinutes();
+let minutes = futureDate.getMinutes();
 
 // the hard picks
 const monthIndex = futureDate.getMonth();
 console.log('Watch out wrong month = ', monthIndex);
 console.log(`The correct month is ${monthIndex} + 1 = ${months[monthIndex]}`);
 const actualMonth = months[monthIndex] ;
+
+const dayOfTheWeekIndex = futureDate.getDay();
+console.log(`JavaScript getDay() brings the day of the week with 0 index based array = ${dayOfTheWeekIndex}`);
+const dayOfTheWeek = weekdays[dayOfTheWeekIndex];
+
+const dayOfTheMonth = futureDate.getDate();
+console.log(dayOfTheMonth);
+
 
 let periodOfDay
 if (hours < 12) {
@@ -54,9 +64,26 @@ if (hours < 12) {
 	periodOfDay = 'pm';
 }
 
-giveawayHeading.textContent = `giveaway ends on ${actualMonth} ${year} ${hours}:${minutes}${periodOfDay}`;
+if (minutes < 10) {
+	minutes = `0${minutes}`;
+}
+
+giveawayHeading.textContent = `giveaway ends on ${dayOfTheWeek}, ${dayOfTheMonth} ${actualMonth} ${year}, ${hours}:${minutes}${periodOfDay}`;
 
 
+// future time in milliseconds
+const futureTime = futureDate.getTime();
+console.log('Time in milliseconds = ', futureTime);
 
+
+function getRemainingTime() {
+	const today = new Date().getTime();
+	console.log(today);
+	
+	const t = futureTime - today;
+	console.log(t);
+};
+
+getRemainingTime();
 
 
