981d412301e070929ae01c43fe83e60b4e4ad2b8
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-12-22 21:14:07 -0300

    Criar variaveis para os elementos html. Usar forEach sobre o Nodelist de slides para setar a propriedade Left. Criar uma funcao que move toda a selecao de slides para a esquerda. Criar listeners para os botoes next e prev e com eles controlar o indice do slide a ser visualizado.

--- 15-slider/app.js
+++ 15-slider/app.js
@@ -1,16 +1,49 @@
 // https://www.youtube.com/watch?v=c5SIG7Ie0dM
-// 8 hs  06'  10''
+// 8 hs  19'  10''
+
+const slides = document.querySelectorAll('.slide');
+const nextBtn = document.querySelector('.next-btn');
+const prevBtn = document.querySelector('.prev-btn');
 
 
 
+slides.forEach(function(slide, index) {
+	slide.style.left = `${index * 100}%`;
+});
 
 
+let counter = 0;
+
+nextBtn.addEventListener('click', function() {
+	counter += 1;
+	
+	carousel();
+});
+
+prevBtn.addEventListener('click', function() {
+	counter -= 1;
+	
+	carousel();
+});
 
 
-
-
-
-
+function carousel() {
+	// first implementation
+	if (counter === slides.length) {
+		counter = 0;
+	}
+	if (counter < 0) {
+		counter = slides.length - 1;
+	}
+	
+	/*
+		Moving all the slides to the left: 
+		translateX(-100%)
+	*/
+	slides.forEach(function(slide) {
+		slide.style.transform = `translateX(-${counter * 100}%)`;
+	});
+};
 
 
 
